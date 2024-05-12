d6cbe8bbeab599ccfe7af2dcb05769b012dc306a
Muhammad Yusuf <muhammadyusuf@MacBook-Air-Muhammad.local>
2024-05-12 10:10:45 +0700

    delete unused file

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,12 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MemberModule } from './module/member.module';
 import { BookModule } from './module/book.module';
-import { TransactionModule } from './module/transaction.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/eigen'),
     MemberModule,
     BookModule,
-    TransactionModule
   ],
   controllers: [AppController],
   providers: [AppService],
