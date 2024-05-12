130998219de675b6a96bc6768d3a0fb2d90efcf4
Muhammad Yusuf <muhammadyusuf@MacBook-Air-Muhammad.local>
2024-05-12 10:51:31 +0700

    working on creating dotenv file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MemberModule } from './module/member.module';
@@ -7,7 +8,10 @@ import { BookModule } from './module/book.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/eigen'),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI),
     MemberModule,
     BookModule,
   ],
